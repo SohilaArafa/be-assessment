bc8797721efe2cf93b24fa4ff5585f98d9b59230
SohilaArafa <sohilaarafa2001@gmail.com>
2023-08-12 21:20:35 +0300

    remove comment

--- Tests/UserTests/userTests.test.js
+++ Tests/UserTests/userTests.test.js
@@ -20,7 +20,6 @@ describe("User Registeration", () => {
 });
 
 describe("User SignIn", () => {
-
   it("Should return a 400 status code for missing email or password", async () => {
     const res = await requestWithSupertest.post("/signIn").send({
       email: "example@gmail.com",
@@ -29,17 +28,7 @@ describe("User SignIn", () => {
     expect(res.statusCode).equal(400);
   });
 
-  // it("Should return a 200 status code for valid credentials", async function () {
-  //   this.timeout(5000);
-
-  //   const res = await requestWithSupertest.post("/signIn").send({
-  //     email: "example@gmail.com",
-  //     password: "example@1234",
-  //   });
-
-  //   expect(res.statusCode).equal(200);
-  // });
-  // it("User sign up", async () => {
+  // it("User Registeration", async () => {
   //   this.timeout(5000);
 
   //   const res = await requestWithSupertest.delete("/delete").send({
@@ -53,29 +42,3 @@ describe("User SignIn", () => {
   //   expect(res2.statusCode).equal(200);
   // });
 });
-
-// describe("User Registration API", () => {
-//   let sandbox;
-
-//   before(() => {
-//     sandbox = sinon.createSandbox();
-//   });
-
-//   after(() => {
-//     sandbox.restore();
-//   });
-
-//   it("should return a 234 status code if email was already registered", async () => {
-//     // Stub the User.findOne() method to simulate an already registered email
-//     sandbox.stub(User, "register").resolves({ email: "example@gmail.com" });
-
-//     const response = await request(app)
-//       .post("/register")
-//       .send({
-//         email: "example@gmail.com",
-//         password: "123",
-//       });
-
-//     expect(response.status).to.equal(234); // Check if the status code is 234
-//   });
-// });
